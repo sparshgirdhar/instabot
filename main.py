121e2c74a3e44737bc8c2c61bb5c81776a41b1d4
sparsh <sparshgirdhar19@gmail.com>
2017-07-02 12:29:29 +0530

    Delete function added

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests, urllib
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
 
 access_token= "2177622775.bcc91a8.70838b84b2ac4fc09d68e2a2ef9b1981"
 
@@ -8,7 +10,6 @@ def self_info():
     request_url = (base_url + 'users/self/?access_token=%s') % (access_token)
     print "GET request url : %s" % (request_url)
     user_info = requests.get(request_url).json()
-    print user_info
 
     if user_info['meta']['code'] == 200:
         if len(user_info['data']):
@@ -93,4 +94,79 @@ def get_user_post(insta_username):
     else:
         print 'Status code other than 200 received!'
 
-get_user_post(insta_username)
+def get_post_id(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (base_url + 'users/%s/media/recent/?access_token=%s') % (user_id, access_token)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
+
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']):
+            return user_media['data'][0]['id']
+        else:
+            print 'There is no recent post of the user!'
+            exit()
+    else:
+        print 'Status code other than 200 received!'
+        exit()
+
+def like_a_post(insta_username):
+    media_id = get_post_id(insta_username)
+    request_url = (base_url + "media/%s/likes") %(media_id)
+    payload = {"access_token": access_token}
+    print 'POST request url : %s' % (request_url)
+    post_like= requests.post(request_url, payload).json()
+
+    if post_like['meta']['code'] == 200:
+        print 'Like was successful!'
+    else:
+        print 'Your like was unsuccessful. Try again!'
+
+def post_a_comment(insta_username):
+    media_id = get_post_id(insta_username)
+    comment_text = raw_input("Your comment: ")
+    payload = {"access_token": access_token, "text" : comment_text}
+    request_url = (base_url + 'media/%s/comments') % (media_id)
+    print 'POST request url : %s' % (request_url)
+
+    make_comment = requests.post(request_url, payload).json()
+
+    if make_comment['meta']['code'] == 200:
+        print "Successfully added a new comment!"
+    else:
+        print "Unable to add comment. Try again!"
+
+def delete_negative_comment(insta_username):
+    media_id = get_post_id(insta_username)
+    request_url = (base_url + 'media/%s/comments/?access_token=%s') % (media_id, access_token)
+    print 'GET request url : %s' % (request_url)
+    comment_info = requests.get(request_url).json()
+
+    if comment_info['meta']['code'] == 200:
+        if len(comment_info['data']):
+            #Here's a naive implementation of how to delete the negative comments :)
+            for x in range(0, len(comment_info['data'])):
+                comment_id = comment_info['data'][x]['id']
+                comment_text = comment_info['data'][x]['text']
+                blob = TextBlob(comment_text, analyzer=NaiveBayesAnalyzer())
+                if (blob.sentiment.p_neg > blob.sentiment.p_pos):
+                    print 'Negative comment : %s' % (comment_text)
+                    delete_url = (base_url + 'media/%s/comments/%s/?access_token=%s') % (media_id, comment_id, access_token)
+                    print 'DELETE request url : %s' % (delete_url)
+                    delete_info = requests.delete(delete_url).json()
+
+                    if delete_info['meta']['code'] == 200:
+                        print 'Comment successfully deleted!\n'
+                    else:
+                        print 'Unable to delete comment!'
+                else:
+                    print 'Positive comment : %s\n' % (comment_text)
+        else:
+            print 'There are no existing comments on the post!'
+    else:
+        print 'Status code other than 200 received!'
+
+delete_negative_comment(insta_username)
